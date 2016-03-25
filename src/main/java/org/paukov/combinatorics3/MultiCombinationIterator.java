af3f4e7141374db42b00282b1493b7cb1742f880
dpaukov <d.paukov@gmail.com>
2016-03-24 22:14:14 -0700

    Refactor the multi combination iterator

--- src/main/java/org/paukov/combinatorics3/MultiCombinationIterator.java
+++ src/main/java/org/paukov/combinatorics3/MultiCombinationIterator.java
@@ -21,34 +21,14 @@ import java.util.List;
 class MultiCombinationIterator<T> implements
 		Iterator<List<T>> {
 
-	/**
-	 * Generator
-	 */
     final MultiCombinationGenerator<T> generator;
-
-	/**
-	 * Current combination
-	 */
     List<T> currentCombination = null;
 
-	/**
-	 * Index of the current combination
-	 */
+
     long currentIndex = 0;
-
-	/**
-	 * Size of the original vector/set
-	 */
     final int lengthN;
-
-	/**
-	 * Size of the combinations (number of elements) to generate
-	 */
     final int lengthK;
 
-	/**
-	 * A helper array
-	 */
 	private int[] bitVector = null;
 
 	/**
@@ -56,12 +36,7 @@ class MultiCombinationIterator<T> implements
 	 */
 	private boolean end = false;
 
-	/**
-	 * Constructor
-	 *
-	 * @param generator
-	 *            Multi-combinations generator
-	 */
+
 	public MultiCombinationIterator(MultiCombinationGenerator<T> generator) {
 		this.generator = generator;
 		lengthN = generator.originalVector.size();
@@ -76,18 +51,12 @@ class MultiCombinationIterator<T> implements
 	}
 
 
-
-	/**
-	 * Returns true if all combinations were iterated, otherwise false
-	 */
 	@Override
 	public boolean hasNext() {
 		return (end != true);
 	}
 
-	/**
-	 * Moves to the next combination
-	 */
+
 	@Override
 	public List<T> next() {
 		currentIndex++;
@@ -130,7 +99,7 @@ class MultiCombinationIterator<T> implements
 			end = true;
 		}
 
-		// return the current combination
+		// return a copy of the current combination
 		return new ArrayList<>(currentCombination);
 	}
 
@@ -139,11 +108,7 @@ class MultiCombinationIterator<T> implements
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * Returns the current combination as a string
-	 *
-	 * @see Object#toString()
-	 */
+
 	@Override
 	public String toString() {
 		return "MultiCombinationIterator=[#" + currentIndex + ", "
@@ -151,7 +116,7 @@ class MultiCombinationIterator<T> implements
 	}
 
     private static <T> void setValue(List<T> list, int index, T value) {
-        if (list.size() < index) {
+        if (index < list.size()) {
             list.set(index, value);
         } else {
             list.add(index, value);
