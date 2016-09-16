e8f6811651c67b06ae158b8096182a549ea4190b
dpaukov <d.paukov@gmail.com>
2016-09-16 10:34:36 -0700

    Refactor the integer partition iterator

--- src/main/java/org/paukov/combinatorics3/IntegerPartitionIterator.java
+++ src/main/java/org/paukov/combinatorics3/IntegerPartitionIterator.java
@@ -5,105 +5,104 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Created by dima on 9/16/16.
+ * Created by dpaukov on 9/16/16.
  */
 class IntegerPartitionIterator implements Iterator<List<Integer>> {
 
 
     final Integer initialValue;
-    List<Integer> _currentPartition = null;
+    List<Integer> currentPartition = null;
 
-    long _currentIndex = 0;
+    long currentIndex = 0;
 
     /**
      * Helper vectors
      */
-    private int[] _mVector = null;
-    private int[] _zVector = null;
+    private int[] mVector = null;
+    private int[] zVector = null;
 
     /**
      * Stop criteria
      */
-    private int _kIndex = 1;
+    private int kIndex = 1;
 
     public IntegerPartitionIterator(IntegerPartitionGenerator generator) {
 
         initialValue = generator.value;
-        _mVector = new int[initialValue + 2];
-        _zVector = new int[initialValue+ 2];
+        mVector = new int[initialValue + 2];
+        zVector = new int[initialValue+ 2];
 
         if (initialValue > 0) {
-            _currentIndex = 0;
-            _kIndex = 1;
+            currentIndex = 0;
+            kIndex = 1;
 
-            setInternalVectorValue(-1, _zVector, 0);
-            setInternalVectorValue(-1, _mVector, 0);
+            setInternalVectorValue(-1, zVector, 0);
+            setInternalVectorValue(-1, mVector, 0);
 
-            setInternalVectorValue(0, _zVector, initialValue + 1);
-            setInternalVectorValue(0, _mVector, 0);
+            setInternalVectorValue(0, zVector, initialValue + 1);
+            setInternalVectorValue(0, mVector, 0);
 
-            setInternalVectorValue(1, _zVector, 1);
-            setInternalVectorValue(1, _mVector, initialValue);
+            setInternalVectorValue(1, zVector, 1);
+            setInternalVectorValue(1, mVector, initialValue);
         } else {
-            _kIndex = 0;
+            kIndex = 0;
         }
     }
 
 
     @Override
     public boolean hasNext() {
-        return _kIndex != 0;
+        return kIndex != 0;
     }
 
 
     @Override
     public List<Integer> next() {
-        _currentIndex++;
-        createCurrentPartition(_kIndex);
-        int sum = getInternalVectorValue(_kIndex, _mVector)
-                * getInternalVectorValue(_kIndex, _zVector);
-        if (getInternalVectorValue(_kIndex, _mVector) == 1) {
-            _kIndex--;
-            sum += getInternalVectorValue(_kIndex, _mVector)
-                    * getInternalVectorValue(_kIndex, _zVector);
+        currentIndex++;
+        currentPartition = createCurrentPartition(kIndex, mVector, zVector);
+        int sum = getInternalVectorValue(kIndex, mVector)
+                * getInternalVectorValue(kIndex, zVector);
+        if (getInternalVectorValue(kIndex, mVector) == 1) {
+            kIndex--;
+            sum += getInternalVectorValue(kIndex, mVector)
+                    * getInternalVectorValue(kIndex, zVector);
         }
-        if (getInternalVectorValue(_kIndex - 1, _zVector) == getInternalVectorValue(
-                _kIndex, _zVector) + 1) {
-            _kIndex--;
-            setInternalVectorValue(_kIndex, _mVector,
-                    getInternalVectorValue(_kIndex, _mVector) + 1);
+        if (getInternalVectorValue(kIndex - 1, zVector) == getInternalVectorValue(
+                kIndex, zVector) + 1) {
+            kIndex--;
+            setInternalVectorValue(kIndex, mVector,
+                    getInternalVectorValue(kIndex, mVector) + 1);
         } else {
-            setInternalVectorValue(_kIndex, _zVector,
-                    getInternalVectorValue(_kIndex, _zVector) + 1);
-            setInternalVectorValue(_kIndex, _mVector, 1);
+            setInternalVectorValue(kIndex, zVector,
+                    getInternalVectorValue(kIndex, zVector) + 1);
+            setInternalVectorValue(kIndex, mVector, 1);
         }
-        if (sum > getInternalVectorValue(_kIndex, _zVector)) {
-            setInternalVectorValue(_kIndex + 1, _zVector, 1);
-            setInternalVectorValue(_kIndex + 1, _mVector, sum
-                    - getInternalVectorValue(_kIndex, _zVector));
-            _kIndex++;
+        if (sum > getInternalVectorValue(kIndex, zVector)) {
+            setInternalVectorValue(kIndex + 1, zVector, 1);
+            setInternalVectorValue(kIndex + 1, mVector, sum
+                    - getInternalVectorValue(kIndex, zVector));
+            kIndex++;
         }
 
         // return the current partition
-        return _currentPartition;
+        return currentPartition;
     }
 
-
-
-    private void createCurrentPartition(int k) {
-        _currentPartition = new ArrayList<>();
+    private static List<Integer> createCurrentPartition(int k, int[] mVector, int[] zVector) {
+        List<Integer> list = new ArrayList<>();
         for (int index = 1; index <= k; index++) {
-            for (int j = 0; j < getInternalVectorValue(index, _mVector); j++) {
-                _currentPartition.add(getInternalVectorValue(index, _zVector));
+            for (int j = 0; j < getInternalVectorValue(index, mVector); j++) {
+                list.add(getInternalVectorValue(index, zVector));
             }
         }
+        return list;
     }
 
-    private final int getInternalVectorValue(int index, int[] vector) {
+    private static int getInternalVectorValue(int index, int[] vector) {
         return vector[index + 1];
     }
 
-    private final void setInternalVectorValue(int index, int[] vector, int value) {
+    private static void setInternalVectorValue(int index, int[] vector, int value) {
         vector[index + 1] = value;
     }
 
@@ -114,7 +113,7 @@ class IntegerPartitionIterator implements Iterator<List<Integer>> {
 
     @Override
     public String toString() {
-        return "PartitionIterator=[#" + _currentIndex + ", "
-                + _currentPartition + "]";
+        return "PartitionIterator=[#" + currentIndex + ", "
+                + currentPartition + "]";
     }
 }
